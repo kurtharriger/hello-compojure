dc6c8cfa8bcec0adecf2aff5b3a68927ada877bd
Kurt Harriger <kurtharriger@gmail.com>
2010-12-12 12:16:21 -0700

    simple calculator

--- src/Hello/core.clj
+++ src/Hello/core.clj
@@ -1,9 +1,40 @@
 (ns Hello.core
   (:use compojure.core
-        hiccup.core))
+        hiccup.core
+        hiccup.page-helpers))
 
-(defroutes app
-  (GET "/" [] (html [:h1 "Hello World"]))
-  (GET "/:name" [name] (html [:h1 "Hello " name]))
-)
+        (defn view-layout [& content]
+          (html
+            (doctype :xhtml-strict)
+            (xhtml-tag "en"
+              [:head
+                [:meta {:http-equiv "Content-type"
+                        :content "text/html; charset=utf-8"}]
+                [:title "adder"]]
+              [:body content])))
 
+        (defn view-input []
+          (view-layout
+            [:h2 "add two numbers"]
+            [:form {:method "post" :action "/"}
+              [:input.math {:type "text" :name "a"}] [:span.math " + "]
+              [:input.math {:type "text" :name "b"}] [:br]
+              [:input.action {:type "submit" :value "add"}]]))
+
+        (defn view-output [a b sum]
+          (view-layout
+            [:h2 "two numbers added"]
+            [:p.math a " + " b " = " sum]
+            [:a.action {:href "/"} "add more numbers"]))
+
+        (defn parse-input [a b]
+          [(Integer/parseInt a) (Integer/parseInt b)])
+
+        (defroutes app
+          (GET "/" []
+            (view-input))
+
+          (POST "/" [a b]
+            (let [[a b] (parse-input a b)
+                  sum   (+ a b)]
+              (view-output a b sum))))
