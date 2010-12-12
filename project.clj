b1ec7f80e839e28908702215894725aaef16af25
Kurt Harriger <kurtharriger@gmail.com>
2010-12-12 13:01:00 -0700

    main interfers with jetty disable for now

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject Hello "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :main script.run
+;;   :main script.run
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.5.2"]
