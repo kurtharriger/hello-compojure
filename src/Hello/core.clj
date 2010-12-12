883d505071860b30ed98d5e91f0b4ff4f9986c2f
Kurt Harriger <kurtharriger@gmail.com>
2010-12-12 12:42:06 -0700

    decrease indent

--- src/Hello/core.clj
+++ src/Hello/core.clj
@@ -5,45 +5,45 @@
         ring.middleware.reload
         ring.middleware.stacktrace))
 
-        (defn view-layout [& content]
-          (html
-            (doctype :xhtml-strict)
-            (xhtml-tag "en"
-              [:head
-                [:meta {:http-equiv "Content-type"
-                        :content "text/html; charset=utf-8"}]
-                [:title "adder"]]
-              [:body content])))
+(defn view-layout [& content]
+  (html
+    (doctype :xhtml-strict)
+    (xhtml-tag "en"
+      [:head
+        [:meta {:http-equiv "Content-type"
+                :content "text/html; charset=utf-8"}]
+        [:title "adder"]]
+      [:body content])))
 
-        (defn view-input []
-          (view-layout
-            [:h2 "add two numbers"]
-            [:form {:method "post" :action "/"}
-              [:input.math {:type "text" :name "a"}] [:span.math " + "]
-              [:input.math {:type "text" :name "b"}] [:br]
-              [:input.action {:type "submit" :value "add"}]]))
+(defn view-input []
+  (view-layout
+    [:h2 "add two numbers"]
+    [:form {:method "post" :action "/"}
+      [:input.math {:type "text" :name "a"}] [:span.math " + "]
+      [:input.math {:type "text" :name "b"}] [:br]
+      [:input.action {:type "submit" :value "add"}]]))
 
-        (defn view-output [a b sum]
-          (view-layout
-            [:h2 "two numbers added"]
-            [:p.math a " + " b " = " sum]
-            [:a.action {:href "/"} "add more numbers"]))
+(defn view-output [a b sum]
+  (view-layout
+    [:h2 "two numbers added"]
+    [:p.math a " + " b " = " sum]
+    [:a.action {:href "/"} "add more numbers"]))
 
-        (defn parse-input [a b]
-          [(Integer/parseInt a) (Integer/parseInt b)])
+(defn parse-input [a b]
+  [(Integer/parseInt a) (Integer/parseInt b)])
 
-        (defroutes handler
-          (GET "/" []
-            (view-input))
+(defroutes handler
+  (GET "/" []
+    (view-input))
 
-          (POST "/" [a b]
-            (let [[a b] (parse-input a b)
-                  sum   (+ a b)]
-              (view-output a b sum))))
+  (POST "/" [a b]
+    (let [[a b] (parse-input a b)
+          sum   (+ a b)]
+      (view-output a b sum))))
 
 
-        (def app
-          (-> #'handler
-            (wrap-reload '[Hello.core])
-            (wrap-stacktrace)))
+(def app
+  (-> #'handler
+    (wrap-reload '[Hello.core])
+    (wrap-stacktrace)))
 
